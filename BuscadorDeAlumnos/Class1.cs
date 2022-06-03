f783d000392da0cc19de23f7f5b08e175cb74e24
MarielaCalcina <marielacalcinaali@gmail.com>
2022-06-02 23:07:32 -0300

    initial commit

---
+++ BuscadorDeAlumnos/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscadorDeAlumnos
+{
+    class Alumnos 
+    {
+        //Atributos 
+        public string nombre;
+        public int numeroIdentificacion;
+        public bool activo;
+        public string notas; 
+
+        //constructor
+        public Alumnos(string nombre, int numeroIdentificacion, bool activo, string notas)//antes era int[]
+        {
+            this.nombre = nombre;
+            this.numeroIdentificacion = numeroIdentificacion;
+            this.activo = activo;
+            this.notas = notas;
+        }
+
+        //Metodos 
+
+        //metodo para calcular el promedio de cada alumno e intento de encontrar el mayor.
+        public int Promedio()
+        {
+            string[] separarNotas = notas.Split(',');
+            string nota1String = separarNotas[0];
+            string nota2String = separarNotas[1];
+            string nota3String = separarNotas[2];
+
+            int nota1 = Int32.Parse(nota1String);
+            int nota2 = Int32.Parse(nota2String);
+            int nota3 = Int32.Parse(nota3String);
+
+            int Promedio = (nota1+ nota2 + nota3)/3;
+
+            var max = 0;
+
+            List<int> notasDeAlumnos = new List<int>
+            { Promedio
+            };
+
+            max = notasDeAlumnos.Max();
+            return max;
+        }
+
+        //metodo para mostrar cada una de las notas del alumno solicitado
+        public string Notas()
+        {
+            string[] separarNotas = notas.Split(",");
+            string nota1String = separarNotas[0];
+            string nota2String = separarNotas[1];
+            string nota3String = separarNotas[2];
+
+            int nota1 = Int32.Parse(nota1String);
+            int nota2 = Int32.Parse(nota2String);
+            int nota3 = Int32.Parse(nota3String);
+
+            string notasTodas = "Las notas del alumno son " + nota1 + " " + nota2 + " " + nota3;
+            return notasTodas;
+        }
+
+        //metodo que nos devuelve el numero de identificacion de cada alumno
+        public int NumeroDeIdentificacion()
+        {
+            return this.numeroIdentificacion;
+        }
+
+        //metodo que nos devuelve el nombre del alumno solicitado
+        public string decirNombre()
+        {
+            return this.nombre;
+        }
+
+        //metodo que nos dice si el alumno esta activo o no
+        public string AlumnoActivoNoActivo()
+        {
+            if (activo == true)
+            {
+                string alumnoActivo= "El alumno " + nombre + " esta activo";
+                return alumnoActivo;
+            }
+            else
+            {
+                string noActivo = "El alumno " + nombre + " no esta activo";
+                return noActivo;
+            }
+        }
+    }
+}
---
+++ BuscadorDeAlumnos/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuscadorDeAlumnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumnos Adrien = new Alumnos("Adrien", 1, true, "6,6,6");
+            Alumnos Jeremy = new Alumnos("Jeremy", 2, true, "8,7,6");
+            Alumnos Rachel = new Alumnos("Rachel", 3, false, "4,7,8");
+            Alumnos Bam = new Alumnos("Bam", 4, true, "10,8,9");
+
+            List<Alumnos> listaDeAlumnos = new List<Alumnos>();
+            listaDeAlumnos.Add(Adrien);
+            listaDeAlumnos.Add(Jeremy);
+            listaDeAlumnos.Add(Rachel);
+            listaDeAlumnos.Add(Bam);
+            
+            
+            //En este caso se presenta una funcion en donde se le solicita al usuario un nombre para poder buscarlo en los datos
+            //que se tienen almacenados
+            Console.WriteLine("Ingrese el nombre del alumno a buscar");
+            string nombreIngresado = Console.ReadLine();
+            string nombresEnCadaVuelta;
+
+            foreach (Alumnos listaAlumnos in listaDeAlumnos)
+            {
+                nombresEnCadaVuelta = listaAlumnos.decirNombre();
+                if (nombreIngresado.ToUpper() == nombresEnCadaVuelta.ToUpper())
+                {
+                    Console.WriteLine("El alumno si se encuentra registrado");
+                }
+                else
+                    Console.WriteLine("No pudo hallarse el alumno solicitado");
+                {
+                }
+            } 
+
+
+
+            //Funcion buscadora por numero de identificacion que ingrese el usuario
+            Console.WriteLine("Ingrese el numero de identificación del alumno");
+            int numeroDeIdentificaciónIngresado = int.Parse(Console.ReadLine());
+
+            int numeroIDEnCadaVuelta;
+
+            foreach (Alumnos numeroDeIdentificacion in listaDeAlumnos)
+            {
+                numeroIDEnCadaVuelta = numeroDeIdentificacion.NumeroDeIdentificacion();
+                if (numeroDeIdentificaciónIngresado == numeroIDEnCadaVuelta)
+                {
+                    Console.WriteLine("El alumno solicitado es " + numeroDeIdentificacion.decirNombre());
+                }
+                else
+                {
+                    Console.WriteLine("No pudo hallarse el alumno solicitado");
+                }
+            }
+
+            //Funcion que muestra a los alumnos y su estado (activos o no activos)
+            foreach (Alumnos estado in listaDeAlumnos)
+            {
+                Console.WriteLine(estado.AlumnoActivoNoActivo());
+            }
+
+        }
+    }
+}
